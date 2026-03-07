95680c9f2561cf8786af19aedd7caec8e2562567
IPochynyukCoding <157660033+IPochynyukCoding@users.noreply.github.com>
2026-03-06 19:59:23 -0600

    Added international and local flags for clearer output

--- usphonechecker.py
+++ usphonechecker.py
@@ -17,10 +17,12 @@ if __name__ == "__main__":
         phone_formats=json.load(phone_format)
     while True:
         phone_number=input("Enter your phone number or press 'q' to quit: ")
+        is_international=False
         if phone_number.lower() == "q":
             quit()
         elif phone_number.startswith("+1"):
-            is_valid_phone_number=phone_checker(phone_number,phone_formats,True)
+            is_international=True
+            is_valid_phone_number=phone_checker(phone_number,phone_formats,is_international)
         else:
             is_valid_phone_number=phone_checker(phone_number,phone_formats)
-        print(f"Your phone number {phone_number} is{"" if is_valid_phone_number else "not"} a valid phone number.")
+        print(f"Your phone number {phone_number} {"is" if is_valid_phone_number else "is not"} a valid {"international" if is_international else "local"} phone number.")
